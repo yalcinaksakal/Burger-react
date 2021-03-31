67018623e979f8b2de67bfaab81aaeed4c636e50
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-03-31 20:19:05 +0300

    firebase2

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -17,12 +17,36 @@ const INGREDIENT_PRICES = {
 
 class BurgerBuilder extends Component {
   state = {
-    ingredients: { salad: 0, bacon: 0, cheese: 0, meat: 0 },
+    ingredients: null,
     totalPrice: 4,
     purchasing: false,
     loading: false,
     error: null,
   };
+
+  priceCalculator(ingredients) {
+    return Object.keys(ingredients).reduce(
+      (price, current) =>
+        (price += ingredients[current] * INGREDIENT_PRICES[current]),
+      4
+    );
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await fetch(baseURL + "ingredients.json");
+
+      if (!response.ok) throw new Error("Could not get response from server.");
+      const data = await response.json();
+      this.setState({
+        ingredients: data,
+        totalPrice: this.priceCalculator(data),
+      });
+    } catch (error) {
+      error.message = "While getting ingredients - " + error.message;
+      this.setState({ error: error });
+    }
+  }
 
   ingredientChangeHandler = (type, opperation) => {
     const oldCount = this.state.ingredients[type];
@@ -73,23 +97,47 @@ class BurgerBuilder extends Component {
         });
       })
       .catch(error => {
+        error.message = "While uploading order - " + error.message;
         this.setState({ loading: false, purchasing: false, error: error });
       });
   };
   render() {
     const disabledInfo = { ...this.state.ingredients };
     for (let key in disabledInfo) disabledInfo[key] = disabledInfo[key] <= 0;
-
-    const orderSummary = this.state.loading ? (
-      <Spinner />
-    ) : (
-      <OrderSummary
-        ingredients={this.state.ingredients}
-        price={this.state.totalPrice}
-        purchaseCanceled={this.purchaseCancelHandler}
-        purchaseContinue={this.purchaseContinueHandler}
-      />
+    let orderSummary = null;
+    let burger = (
+      <div
+        style={{
+          margin: "0 auto",
+          width: "60%",
+          backgroundColor: "whitesmoke",
+        }}
+      >
+        <Spinner />
+      </div>
     );
+    if (this.state.ingredients) {
+      burger = (
+        <Aux>
+          <BuildControls
+            ordered={this.purchaseHandler}
+            ingredientChanged={this.ingredientChangeHandler}
+            disabled={disabledInfo}
+            price={this.state.totalPrice}
+          />{" "}
+          <Burger ingredients={this.state.ingredients} />
+        </Aux>
+      );
+      orderSummary = (
+        <OrderSummary
+          ingredients={this.state.ingredients}
+          price={this.state.totalPrice}
+          purchaseCanceled={this.purchaseCancelHandler}
+          purchaseContinue={this.purchaseContinueHandler}
+        />
+      );
+    }
+    if (this.state.loading) orderSummary = <Spinner />;
     return (
       <Aux>
         <Modal
@@ -99,15 +147,9 @@ class BurgerBuilder extends Component {
           {orderSummary}
         </Modal>
         <Modal show={this.state.error} modalClosed={this.purchaseCancelHandler}>
-          {`Something went wrong. (${this.state.error})`}
+          {`❌ Something went wrong. (${this.state.error})`}
         </Modal>
-        <BuildControls
-          ordered={this.purchaseHandler}
-          ingredientChanged={this.ingredientChangeHandler}
-          disabled={disabledInfo}
-          price={this.state.totalPrice}
-        />{" "}
-        <Burger ingredients={this.state.ingredients} />
+        {burger}
       </Aux>
     );
   }
